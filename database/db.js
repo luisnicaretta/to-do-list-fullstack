06179c72a57d75c380e428dfa8b0bb0c4165fd74
luisnicaretta <luisnicaretta27@gmail.com>
2024-10-24 20:15:54 -0300

    extra configs

--- database/db.js
+++ database/db.js
@@ -1,14 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectToDb = () => {
-    mongoose.connect(
-        "mongodb+srv://root:admin@to-do-list.8knms.mongodb.net/?retryWrites=true&w=majority&appName=to-do-list",
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }
-    ).then(() => console.log("MongoDB Atlas CONECTADO!")
-    ).catch((err) => console.log(err));
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB Atlas CONECTADO!"))
+    .catch((err) => console.log(err));
 };
 
 module.exports = connectToDb;
